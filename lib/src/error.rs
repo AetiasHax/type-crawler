f493d611b3f71344c43b29792b050fb29304e37b
Aetias <aetias@outlook.com>
2025-08-23 14:59:09 +0200

    Remove Backtrace from error

--- lib/src/error.rs
+++ lib/src/error.rs
@@ -1,5 +1,3 @@
-use std::backtrace::Backtrace;
-
 use clang::SourceError;
 use snafu::Snafu;
 
@@ -8,49 +6,42 @@ use crate::ExtendTypesError;
 #[derive(Debug, Snafu)]
 #[snafu(visibility(pub(crate)))]
 pub enum TypeCrawlerError {
-    #[snafu(display("Failed to initialize clang: {message}:\n{backtrace}"))]
-    ClangInit { message: String, backtrace: Backtrace },
+    #[snafu(display("Failed to initialize clang: {message}"))]
+    ClangInit { message: String },
 }
 
 #[derive(Debug, Snafu)]
 #[snafu(visibility(pub(crate)))]
 pub enum AddIncludePathError {
-    #[snafu(display("Path does not exist: {path}:\n{backtrace}"))]
-    DoesNotExist { path: String, backtrace: Backtrace },
-    #[snafu(display("Path is not a directory: {path}:\n{backtrace}"))]
-    NotADirectory { path: String, backtrace: Backtrace },
+    #[snafu(display("Path does not exist: {path}"))]
+    DoesNotExist { path: String },
+    #[snafu(display("Path is not a directory: {path}"))]
+    NotADirectory { path: String },
 }
 
 #[derive(Debug, Snafu)]
 #[snafu(visibility(pub(crate)))]
 pub enum ParseError {
-    #[snafu(display("File not found: {name}:\n{backtrace}"))]
-    FileNotFound { name: String, backtrace: Backtrace },
-    #[snafu(display("Failed to read file: {path}:\n{backtrace}"))]
-    ReadError { path: String, backtrace: Backtrace },
+    #[snafu(display("File not found: {name}"))]
+    FileNotFound { name: String },
+    #[snafu(display("Failed to read file: {path}"))]
+    ReadError { path: String },
     #[snafu(transparent)]
-    ParseError { source: SourceError, backtrace: Backtrace },
-    #[snafu(display("Invalid AST: {message}:\n{backtrace}"))]
-    InvalidAst { message: String, backtrace: Backtrace },
-    #[snafu(display("Unsupported type: {message}:\n{backtrace}"))]
-    UnsupportedType { message: String, backtrace: Backtrace },
-    #[snafu(display("Unsupported entity in {at}: {message}:\n{backtrace}"))]
-    UnsupportedEntity { at: String, message: String, backtrace: Backtrace },
-    #[snafu(display(
-        "Failed to get field offset of {field_name} in {struct_name}: {error}:\n{backtrace}"
-    ))]
-    Offsetof {
-        field_name: String,
-        struct_name: String,
-        error: clang::OffsetofError,
-        backtrace: Backtrace,
-    },
-    #[snafu(display("Failed to get size of type {type_name}: {error}:\n{backtrace}"))]
-    Sizeof { type_name: String, error: clang::SizeofError, backtrace: Backtrace },
-    #[snafu(display("Failed to get alignment of type {type_name}: {error}:\n{backtrace}"))]
-    Alignof { type_name: String, error: clang::AlignofError, backtrace: Backtrace },
-    #[snafu(display("Invalid fields in {struct_name}: {field_names:?}:\n{backtrace}"))]
-    InvalidFields { field_names: Vec<String>, struct_name: String, backtrace: Backtrace },
+    ParseError { source: SourceError },
+    #[snafu(display("Invalid AST: {message}"))]
+    InvalidAst { message: String },
+    #[snafu(display("Unsupported type: {message}"))]
+    UnsupportedType { message: String },
+    #[snafu(display("Unsupported entity in {at}: {message}"))]
+    UnsupportedEntity { at: String, message: String },
+    #[snafu(display("Failed to get field offset of {field_name} in {struct_name}: {error}"))]
+    Offsetof { field_name: String, struct_name: String, error: clang::OffsetofError },
+    #[snafu(display("Failed to get size of type {type_name}: {error}"))]
+    Sizeof { type_name: String, error: clang::SizeofError },
+    #[snafu(display("Failed to get alignment of type {type_name}: {error}"))]
+    Alignof { type_name: String, error: clang::AlignofError },
+    #[snafu(display("Invalid fields in {struct_name}: {field_names:?}"))]
+    InvalidFields { field_names: Vec<String>, struct_name: String },
     #[snafu(transparent)]
     ExtendTypesError { source: ExtendTypesError },
 }
